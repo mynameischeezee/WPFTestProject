4d1456a1f778493a7986c46e33f3ca829fc87c63
Maksim Zayarnyuk <m.zayarnyuk@itransition.com>
2021-12-02 18:27:30 +0200

    Refactoring

--- BAMTestProject.BL.Implement/Services/BroadcastEndDateCalculator.cs
+++ BAMTestProject.BL.Implement/Services/BroadcastEndDateCalculator.cs
@@ -6,15 +6,14 @@ namespace BAMTestProject.BL.Implementation.Services
 {
     public class BroadcastEndDateCalculator : IBroadcastEndDateCalculator
     {
-        //TODO: Divide methods in ternary operator
         public DateTime Calculate(DateTime startDate, int viewCount, List<DayOfWeek> weekDays)
         {
             var endDate = startDate;
             var amountOfWeeks = GetAmountOfWeeks(viewCount, weekDays);
             var viewsCount = viewCount - amountOfWeeks * weekDays.Count;
             return !IsHasExtraWeek(viewCount, weekDays) 
-                ? endDate.AddDays(amountOfWeeks * 7 + (int)weekDays[viewsCount])
-                : endDate.AddDays(amountOfWeeks * 7);
+                ? OddWeekCalculator(startDate, amountOfWeeks, weekDays, viewsCount)
+                : NonOddWeekCalculator(endDate, amountOfWeeks);
         }
 
         private int GetAmountOfWeeks(int viewCount, List<DayOfWeek> weekDays)
@@ -23,6 +22,15 @@ namespace BAMTestProject.BL.Implementation.Services
             return amountOfWeeks;
         }
 
+        private DateTime OddWeekCalculator(DateTime newDate,int amountOfWeeks, List<DayOfWeek> weekDays, int viewsCount)
+        {
+            return newDate.AddDays(amountOfWeeks * 7 + (int) weekDays[viewsCount]);
+        }
+
+        private DateTime NonOddWeekCalculator(DateTime newDate, int amountOfWeeks)
+        {
+            return newDate.AddDays(amountOfWeeks * 7);
+        }
         private bool IsHasExtraWeek(int viewCount, List<DayOfWeek> weekDays)
         {
             return (viewCount % weekDays.Count) == 0;
